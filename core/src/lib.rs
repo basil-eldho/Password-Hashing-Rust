371d6e68f06a79395fd6bffe0f3fa43b6c1e8b6a
Basil Eldho <basileljol@gmail.com>
2025-04-12 14:10:14 -0400

    fix validation function errors

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -1,9 +1,29 @@
-pub enum TestStruct {
-    Both,
+use tracing::error;
+pub mod args;
+
+pub fn max_threads(s: &str) -> Result<usize, String> {
+    let threads = s.parse_value("Failed to parse the thread count as a number")?;
+
+    let max_available = std::thread::available_parallelism()
+        .map_err(|_| "Failed to determine the number of available CPU cores".to_string())?
+        .get();
+
+    // Validate threads
+    if threads == 0 {
+        error!("Error: Number of threads must be greater than zero.");
+        return Err("Error: Number of threads must be greater than zero.".to_string());
+    }
+
+    if threads > max_available {
+        return Err(format!(
+            "Requested thread count ({}) exceeds the available CPU threads ({})",
+            threads, max_available
+        ));
+    }
+    Ok(threads)
 }
 
-impl TestStruct {
-    pub fn test(self) {
-        println!("I took ownership");
-    }
+pub fn max_num(s: &str) -> Result<usize, String> {
+    let num = s.parse_value("Failed to parse the given number as a number")?;
+    Ok(num)
 }
